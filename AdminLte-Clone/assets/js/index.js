d48f8596a577e6c199844e5f6e8b30afb61d0d7e
jose <josermarinr18@gmail.com>
2021-03-03 20:44:30 +0100

    adding chartjs and finish the table

---
+++ AdminLte-Clone/assets/js/index.js
@@ -0,0 +1,65 @@
+let ctx = document.getElementById('myChart').getContext('2d');
+
+let chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Janv', 'feb', 'mar', 'avr', 'may', 'jul'],
+    datasets: [{
+      label: 'migration of DB',
+      data: [12, 19, 3, 5, 10, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    },{
+      label: 'testing & CI',
+      data:  [12, 17, 5, 8, 2, 10],
+      backgroundColor: [ 
+        'rgba(255, 159, 64, 0.3)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+       
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    },
+  ]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    animation: {
+      duration: 1500,
+      easing:'linear',
+  }
+  }
+});
+console.log(chart.defaults.global.animation);
+chart.canvas.parentNode.style.height = '300px';
+chart.canvas.parentNode.style.width = '600px';
